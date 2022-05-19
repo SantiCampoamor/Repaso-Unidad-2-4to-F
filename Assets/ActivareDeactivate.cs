0cdf153feccc833ba32c9630c324f717033f2091
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-05-19 08:57:17 -0300

    Initial commit

---
+++ Assets/ActivareDeactivate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivareDeactivate : MonoBehaviour
+{
+    public GameObject inputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ActivateAndDeactivate()
+    {
+        //if(inputField.activeInHierarchy)
+        //{
+        //    inputField.SetActive(false);
+        //}
+        //else
+        //{
+        //    inputField.SetActive(true);
+        //}
+        inputField.SetActive(!inputField.activeInHierarchy);
+    }
+}
